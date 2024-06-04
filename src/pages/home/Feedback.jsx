2a92833798eed5360f3da5407f883e581a13eb4a
coder-MuskanRajput <muskanrajput1510@gmail.com>
2024-06-04 22:10:21 +0530

    Feedback

---
+++ src/pages/home/Feedback.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+
+
+const Feedback = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const feedbacks = [
+    {
+      id: 1,
+      text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur velit soluta fugiat et deserunt, sit consequatur rerum possimus ab hic ipsa quisquam repudiandae, molestias facilis consectetur odio. Illum, magnam commodi?',
+      author: "Rohit Muley"
+    },
+    {
+      id: 2,
+      text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur velit soluta fugiat et deserunt, sit consequatur rerum possimus ab hic ipsa quisquam repudiandae, molestias facilis consectetur odio. Illum, magnam commodi?',
+      author: "Abhishek B Jain"
+    },
+    {
+      id: 3,
+      text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur velit soluta fugiat et deserunt, sit consequatur rerum possimus ab hic ipsa quisquam repudiandae, molestias facilis consectetur odio. Illum, magnam commodi?',
+      author: "Rashmi Khandelwal"
+    },
+    {
+      id: 4,
+      text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur velit soluta fugiat et deserunt, sit consequatur rerum possimus ab hic ipsa quisquam repudiandae, molestias facilis consectetur odio. Illum, magnam commodi?',
+      author: "Rohit Muley"
+    },
+    {
+      id: 5,
+      text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur velit soluta fugiat et deserunt, sit consequatur rerum possimus ab hic ipsa quisquam repudiandae, molestias facilis consectetur odio. Illum, magnam commodi?',
+      author: "Abhishek B Jain"
+    },
+    {
+      id: 6,
+      text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur velit soluta fugiat et deserunt, sit consequatur rerum possimus ab hic ipsa quisquam repudiandae, molestias facilis consectetur odio. Illum, magnam commodi?',
+      author: "Rashmi Khandelwal"
+    }
+  ];
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentPage((prevPage) => (prevPage + 1) % (feedbacks.length / 2));
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [feedbacks.length]);
+
+  const handlePagination = (pageIndex) => {
+    setCurrentPage(pageIndex);
+  };
+
+  return (
+    <div className="p-4 relative" style={{ backgroundImage: `url("https://faime-next.vercel.app/_next/static/media/testimonial.9c4d3c06.png")` }}>
+      <div className="absolute inset-0 bg-black opacity-80"></div>
+      <div className="relative text-right text-[#ED1C24] text-2xl  mr-40  mb-5 mt-5">FEEDBACK</div>
+      <div className="relative text-right text-white text-7xl text-bold mb-8 mr-20">Customer Feedback</div>
+      <div className="relative flex justify-start items-center w-full mb-8 pl-8">
+        {Array.from({ length: feedbacks.length / 2 }, (_, index) => (
+          <span
+            key={index}
+            onClick={() => handlePagination(index)}
+            className={`cursor-pointer w-4 h-4 rounded-full mx-1 ${currentPage === index ? 'bg-[#ED1C24]' : 'bg-gray-400'}`}
+          />
+        ))}
+      </div>
+      <div className="relative carousel w-full flex justify-center gap-4 flex-wrap">
+        {feedbacks.slice(currentPage * 2, currentPage * 2 + 2).map(feedback => (
+          <div key={feedback.id} className='w-full rounded-2xl md:w-5/12 h-96 bg-white flex flex-col items-center justify-center p-4'>
+            <div className='text-4xl text-[#ED1C24] mb-4'>
+            <FontAwesomeIcon icon={faComment} className="text-4xl text-[#ED1C24]" />
+
+            </div>
+            <div className='text-center mb-4'>
+              {feedback.text}
+            </div>
+            <div className='text-right font-bold w-full'>- {feedback.author}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Feedback;
--- src/pages/home/HeroPage.jsx
+++ src/pages/home/HeroPage.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 const HeroPage = () => {
     const ourEmplyee = [
         {
-            work: "I'M Script Writer",
-            Name1: 'RUSSO',
-            Name2: 'BROTHERS',
+            work: "I'M Line Producer",
+            Name1: 'Sachin',
+            Name2: 'Sharma',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas asperiores obcaecati culpa, ratione cumque temporibu.',
             img: 'https://faime-next.vercel.app/_next/static/media/director2.ae539b43.jpg'
         },
@@ -74,12 +75,12 @@ const HeroPage = () => {
                             <p className='text-lg tracking-wide text-gray-400 w-[80%]'>{ourEmplyee[isOpen].description}</p>
                         </motion.div>
                     </AnimatePresence>
-                    <button className='px-12 py-3 hover:bg-[#ED1C24] border border-[#dddddd64] rounded my-8 w-fit'>Contact Me</button>
+                    <Link to="/contact" className='px-12 py-3 hover:bg-[#ED1C24] border border-[#dddddd64] rounded my-8 w-fit'>Contact Me</Link>
                 </div>
             </div>
             <div className='md:w-1/2 h-full flex flex-col items-end gap-5 justify-center p-6'>
-                <div onClick={changeUp} className='p-1 px-2 active:scale-95 text-xl font-bold hover:bg-[#ED1C24] cursor-pointer text-white w-fit aspect-square flex items-center justify-center rounded-full border border-[#dddddd64]'><i className='ri-arrow-up-s-line'></i></div>
-                <div onClick={changeDown} className='p-1 px-2 active:scale-95 text-xl font-bold hover:bg-[#ED1C24] cursor-pointer text-white w-fit aspect-square flex items-center justify-center rounded-full border border-[#dddddd64]'><i className='ri-arrow-down-s-line'></i></div>
+                <div onClick={changeUp} className='p-1 px-2 hidden active:scale-95 text-xl font-bold hover:bg-[#ED1C24] cursor-pointer text-white w-fit aspect-square flex items-center justify-center rounded-full border border-[#dddddd64]'><i className='ri-arrow-up-s-line'></i></div>
+                <div onClick={changeDown} className='p-1 px-2 hidden active:scale-95 text-xl font-bold hover:bg-[#ED1C24] cursor-pointer text-white w-fit aspect-square flex items-center justify-center rounded-full border border-[#dddddd64]'><i className='ri-arrow-down-s-line'></i></div>
             </div>
         </div>
         </div>

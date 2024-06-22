a302e984b3a7aacb932c89fc9c4d7320a2bd2d66
coder-MuskanRajput <muskanrajput1510@gmail.com>
2024-06-22 22:43:55 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base:"/mpFilms/",
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.jpeg'],
 });
